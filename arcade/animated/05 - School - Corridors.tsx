2b9eac6c0fe4cba6de4582eb1aea98848d2791d1
sand-hardwaredev <58972883+sand-hardwaredev@users.noreply.github.com>
2023-01-23 15:32:30 +0400

    Add files via upload

---
+++ arcade/animated/05 - School - Corridors.tsx
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="05 - School - Corridors" tilewidth="48" tileheight="48" tilecount="256" columns="16">
+ <image source="../05 - School - Corridors.png" width="768" height="768"/>
+ <tile id="134">
+  <animation>
+   <frame tileid="120" duration="100"/>
+   <frame tileid="120" duration="100"/>
+   <frame tileid="120" duration="100"/>
+   <frame tileid="120" duration="100"/>
+   <frame tileid="120" duration="100"/>
+   <frame tileid="120" duration="100"/>
+   <frame tileid="120" duration="100"/>
+   <frame tileid="134" duration="100"/>
+   <frame tileid="134" duration="100"/>
+   <frame tileid="134" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="150">
+  <animation>
+   <frame tileid="120" duration="100"/>
+   <frame tileid="120" duration="100"/>
+   <frame tileid="120" duration="100"/>
+   <frame tileid="120" duration="100"/>
+   <frame tileid="120" duration="100"/>
+   <frame tileid="120" duration="100"/>
+   <frame tileid="120" duration="100"/>
+   <frame tileid="150" duration="100"/>
+   <frame tileid="150" duration="100"/>
+   <frame tileid="150" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ arcade/animated/0x-arcade-fixed.tsx
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="0x-arcade-fixed" tilewidth="48" tileheight="48" tilecount="8" columns="4">
+ <image source="../0x-arcade-fixed.png" width="192" height="96"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="0" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="1" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="3">
+  <animation>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
